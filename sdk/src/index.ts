f1a0c3278df247c5366b41b7576533d55f558e98
halcn-protocol <halcn-protocol@users.noreply.github.com>
2026-04-02 18:47:34 +0900

    expand SDK exports with all modules

--- sdk/src/index.ts
+++ sdk/src/index.ts
@@ -1,2 +1,19 @@
 export { HalcnClient } from "./client";
-export type { SignalConfig, SignalRecord } from "./types";
+export {
+  computeDecayChain,
+  estimatePropagationLatency,
+  confidenceIntervalWidth,
+  canPropagate,
+} from "./propagation";
+export { PROGRAM_ID, SEEDS, BPS, DEFAULTS } from "./constants";
+export { HalcnSDKError, parseErrorCode } from "./errors";
+export { bpsToDecimal, decimalToBps, formatEta } from "./utils";
+export type {
+  SignalConfig,
+  SignalRecord,
+  PropagationNode,
+  PropagationPathRecord,
+  ImpactPredictionRecord,
+  PreArrivalAlert,
+  HalcnClientConfig,
+} from "./types";
